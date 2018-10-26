ea1cd4e0073c17c9033ee14b73417f1577ed858d
Oriol Fornés <oriol@cmmt.ubc.ca>
2018-10-26 10:38:39 -0700

    updated GUD module

---
+++ GUD/scripts/__init__.py
@@ -0,0 +1,13 @@
+"""
+@file: __init__.py
+
+@author: Oriol Fornes
+@mail: oriol.fornes@gmail.com
+@date: 2018
+
+@[wasserman lab](http://www.cisreg.ca)
+
+@description: Python scripts to help populate GUD
+"""
+__all__ = ["bed2gud", "encode2gud", "initialize_gud_db", "remap2gud", "vista2gud"]
+__version__ = "0.0.1"
