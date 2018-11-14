00cf0c6bf8096287bd808df3625c17314c77f5fc
Oriol Fornés <oriol@cmmt.ubc.ca>
2018-11-14 15:17:39 -0800

    added fantom parser

--- GUD/scripts/fantom2gud.py
+++ GUD/scripts/fantom2gud.py
@@ -986,10 +986,10 @@ def insert_fantom_to_gud_db(user, host, port, db, matrix_file,
             # ... Else...
             else: pass
             for sample in original_sample_names:
-                m = re.search("(CNhs\d+)", sample)
+                m = re.search("CNhs(\d+)", sample)
                 for curated_sample in grouped_sample_names:
-                    n = re.search("(CNhs\d+)", curated_sample)
-                    if m.group(1) == n.group(1):
+                    n = re.search("CNhs(\d+)", curated_sample)
+                    if int(m.group(1)) == int(n.group(1)):
                         print("\"%s\": \"%s\"," % (sample, curated_sample))
             exit(0)
 #                # Initialize
