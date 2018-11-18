6ad669bb1133d752bc8d36c9e6628d829d62fbe9
Oriol Fornes <ofornes@dhcp-206-87-123-177.ubcsecure.wireless.ubc.ca>
2018-11-18 10:29:20 -0800

    converted tpm to array of floats

--- GUD/ORM/tss.py
+++ GUD/ORM/tss.py
@@ -24,7 +24,7 @@ class TSS(Base):
     end = Column("end", mysql.INTEGER(unsigned=True), nullable=False)
     strand = Column("strand", mysql.CHAR(1), nullable=False)
     cell_or_tissue = Column("cell_or_tissue", String(225), nullable=False)
-    tpm = Column("tpm", ARRAY(Float), nullable=False)
+    tpm = Column("tpm", ARRAY(Integer), nullable=False)
     source_name = Column("source_name", String(25), nullable=False)
     date = Column("date", Date(), nullable=True)
 
