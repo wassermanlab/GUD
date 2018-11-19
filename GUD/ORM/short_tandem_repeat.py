61258a87f705ff807cc473dfe2079e53c0cce511
Tamar Av-Shalom <avshalom.tamar0@gmail.com>
2018-11-19 10:24:38 -0800

    initial take on the short tandem repeats

---
+++ GUD/ORM/short_tandem_repeat.py
@@ -0,0 +1,51 @@
+from binning import containing_bins, contained_bins 
+from Bio.SeqFeature import FeatureLocation
+
+from sqlalchemy import (
+    Column, Index, Integer,
+    PrimaryKeyConstraint, String
+)
+
+from datetime import date
+from sqlalchemy.dialects import mysql
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
+
+Base = declarative_base()
+
+
+class ShortTandemRepeat(Base):
+    __tablename__ = 'short_tandem_repeat'
+
+    bin = Column("bin", mysql.SMALLINT(unsigned=True), nullable=False)
+    chrom = Column("chrom", String(30), nullable=False)
+    start = Column("start", mysql.INTEGER(unsigned=True), nullable=False)
+    end = Column("end", mysql.INTEGER(unsigned=True), nullable=False)
+    length = Column("length", mysql.INTEGER(unsigned=True), nullable=False)
+    motif = Column("motif", String(5), nullable=False)
+    pathogenicity = Column("pathogenicity", mysql.INTEGER(
+        unsigned=False), nullable=False)
+    date = Column("date", Date(), nullable=True)
+
+    __table_args__ = (
+
+        PrimaryKeyConstraint(
+            chrom, start, end
+        ),
+
+        Index("ix_str", bin, chrom),
+        Index("ix_str_pathogenicity", pathogenicity)
+
+        {
+            "mysql_engine": "MyISAM",
+            "mysql_charset": "utf8"
+        }
+    )
+
+    def __str__(self):
+        return "{}\t{}\t{}\t{}\t{}".format(
+            self.chrom, self.start, self.end, self.length, self.motif)
+
+    def __repr__(self):
+        return "<ShortTandemRepeat(bin={}, chrom={}, start={}, end={}, length={}, motif={}, pathogenicity={})>".format(
+            self.bin, self.chrom, self.start, self.end, self.length, self.motif, self.pathogenicity)
---
+++ GUD/scripts/str2gud.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+import os, sys, re
+import argparse
+from binning import assign_bin
+from datetime import date
+import getpass
+from sqlalchemy import create_engine
+from sqlalchemy.orm import mapper, scoped_session, sessionmaker
+from sqlalchemy_utils import database_exists
+import warnings
+
+# Import from GUD module
+from GUD import GUDglobals
+from GUD.bin_range import BinRange
+from GUD.ORM.short_tandem_repeat import ShortTandemRepeat
+
+#-------------#
+# Classes     #
+#-------------#
+
+class Model(object): pass
+
+#-------------#
+# Functions   #
+#-------------#
+
+def parse_args():
+    """
+    This function parses arguments provided via the command
+    line using argparse.
+    """
+
+    parser = argparse.ArgumentParser(description="this script inserts short tandem repeat location information")
+
+    parser.add_argument("genome", help="Genome assembly")
+    parser.add_argument("metadata", help="Metadata file")
+    parser.add_argument("bed_file", help="gangSTR bed file")
+
+    # Optional args
+    parser.add_argument("--source", default="gangSTR", help="Source name")
+
+    # MySQL args
+    mysql_group = parser.add_argument_group("mysql arguments")
+    mysql_group.add_argument("-d", "--db",
+        help="Database name (default = input genome assembly)")
+    mysql_group.add_argument("-H", "--host", default="localhost",
+        help="Host name (default = localhost)")
+    mysql_group.add_argument("-P", "--port", default=5506, type=int,
+        help="Port number (default = 5506)")
+    mysql_group.add_argument("-u", "--user", default=getpass.getuser(),
+        help="User name (default = current user)")
+
+    args = parser.parse_args()
+
+    # Set default
+    if not args.db:
+        args.db = args.genome
+
+    return args
+
+def insert_str_to_gud_db(user, host, port, db, genome,
+    metadata_file, bed_file, source_name):
+
+    # Initialize
+    metadata = {}
+    db_name = "mysql://{}:@{}:{}/{}".format(
+        user, host, port, db)
+    if not database_exists(db_name):
+        raise ValueError("GUD db does not exist: %s" % db_name)
+    session = scoped_session(sessionmaker())
+    engine = create_engine(db_name, echo=False)
+    session.remove()
+    session.configure(bind=engine, autoflush=False,
+        expire_on_commit=False)
+    today = str(date.today())
+
+    # Initialize table
+    if not engine.has_table("short_tandem_repeat"):
+        raise ValueError("GUD db does not have \"short_tandem_repeat\" table!")
+    table = ShortTandemRepeat()
+    table.metadata.bind = engine
+    table.metadata.create_all(engine)
+    mapper(Model, table.__table__)
+
+    # parse table 
+    with open(bed_file) as f:
+        for line in f: 
+            split_line = line.split("\t")
+            chrom = str(split_line[0])
+            start = int(split_line[1])
+            end = int(split_line[2])
+            length = int(split_line[3])
+            motif = str(split_line[4])
+            pathogenicity = 0
+
+            model = Model()
+            model.bin             = assign_bin(start, end)
+            model.chrom           = chrom
+            model.start           = start
+            model.end             = end
+            model.length          = length
+            model.motif           = motif
+            model.pathogenicity   = pathogenicity
+            model.date = today
+
+            session.add(model)
+    session.flush()
+    session.commit()
+
+#-------------#
+# Main        #
+#-------------#
+
+if __name__ == "__main__":
+
+    # Parse arguments
+    args = parse_args()
+
+    # Insert ENCODE data to GUD database
+    insert_str_to_gud_db(args.user, args.host, args.port, args.db, args.genome,
+    args.metadata_file, args.bed_file, args.source_name)
