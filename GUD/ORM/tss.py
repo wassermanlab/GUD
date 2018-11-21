62e07ef3ebfaf97f1cab11ba6ae9869c0baf4c99
Oriol Fornés <oriol@cmmt.ubc.ca>
2018-11-21 15:55:06 -0800

    added ability to exclude_non_gene_tss

--- GUD/ORM/tss.py
+++ GUD/ORM/tss.py
@@ -10,7 +10,7 @@ from sqlalchemy import (
 
 from sqlalchemy.dialects import mysql
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql import func
+from sqlalchemy.sql import func, exists
 
 from GUD.ORM.gene import Gene
 
