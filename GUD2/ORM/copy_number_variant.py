278fc3ed5b8bfdab2e44d5aac8ed0340236aa907
Tamar Av-Shalom <avshalom.tamar0@gmail.com>
2018-12-14 12:12:29 -0800

    added support for CNVs

---
+++ GUD2/ORM/copy_number_variant.py
@@ -0,0 +1,85 @@
+from sqlalchemy import (
+    Column, Index, PrimaryKeyConstraint, String, ForeignKey,
+    UniqueConstraint, CheckConstraint, Integer
+)
+from sqlalchemy.dialects import mysql
+from GUD2.ORM.region import Region
+from GUD2.ORM.source import Source
+from GUD2.ORM.base import Base
+from binning import containing_bins, contained_bins, assign_bin
+
+class CNV(Base):
+#chrom  start   end     name    clinical_interpretation variant_type    copy_number
+    __tablename__ = "copy_number_variants"
+
+    uid = Column("uid", mysql.INTEGER(unsigned=True))
+    regionID = Column("regionID", Integer, ForeignKey('regions.uid'), nullable=False)
+    sourceID = Column("sourceID", Integer, ForeignKey('sources.uid'), nullable=False)
+    variant_type = Column("variant_type", String(50), nullable=False)
+    copy_number = Column("copy_number", Integer, nullable=False)
+    clinical_interpretation = Column("clinical_interpretation", String(50), nullable=False)
+
+    __table_args__ = (
+        PrimaryKeyConstraint(uid),
+        UniqueConstraint(regionID, sourceID, variant_type, copy_number, clinical_interpretation),
+
+        Index("ix_cnv", regionID),
+        Index("ix_cnv_uid", uid),
+        Index("ix_cnv_clinical_interpretation", clinical_interpretation),
+
+        {
+            "mysql_engine": "MyISAM",
+            "mysql_charset": "utf8"
+        }
+    )
+
+    @classmethod
+    def select_by_location(cls, session, chrom, start, end):
+        """
+        Query objects based off of their location being within the start only
+        motifs through that  
+         """
+        bins = set(containing_bins(start, end) + contained_bins(start, end))
+        q = session.query(cls, Region).\
+        join().\
+        filter(Region.uid == cls.regionID).\
+        filter(Region.chrom == chrom, Region.end > start, Region.start < end).\
+        filter(Region.bin.in_(bins))
+        return q.all()
+
+    @classmethod
+    def select_by_exact_location(cls, session, chrom, start, end):
+        """
+        Query objects based off of their location being within the start only
+        motifs through that  
+         """
+        bin = assign_bin(start, end)
+        
+        q = session.query(cls, Region).\
+        join().\
+        filter(Region.uid == cls.regionID).\
+        filter(Region.chrom == chrom, Region.start == start, Region.end == end).\
+        filter(Region.bin == bin)
+        return q.first()  
+
+    @classmethod
+    def select_by_uid(cls, session, uid):
+        q = session.query(cls, Region).\
+        join().\
+        filter(Region.uid == cls.regionID).\
+        filter(cls.uid == uid)
+        return q.first()
+
+    @classmethod
+    def is_unique(cls, session, regionID, sourceID, variant_type, copy_number, clinical_interpretation):
+        q = session.query(cls).filter(cls.regionID == regionID, cls.sourceID == sourceID,
+         cls.variant_type == variant_type, cls.copy_number==copy_number, cls.clinical_interpretation==clinical_interpretation)
+        q = q.all()
+        return len(q) == 0
+
+    def __str__(self):
+        return "{}\t{}".format(self.copy_number, self.clinical_significance)
+
+    def __repr__(self):
+        return "<CNV(uid={}, regionID={}, sourceID={}, copy_number={}, clinical_significance={}, variant_type={}yes)>".format(
+            self.uid, self.regionID, self.sourceID, self.copy_number, self.clinical_interpretation, self.variant_type)
---
+++ GUD2/scripts/cnv2gud.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# mysql -u ontarget_w --database tamar_test
+import os
+import sys
+import re
+import argparse
+from binning import assign_bin
+import getpass
+from sqlalchemy import create_engine
+from sqlalchemy.orm import mapper, scoped_session, sessionmaker
+from sqlalchemy_utils import database_exists
+import warnings
+
+# Import from GUD module
+from GUD2 import GUDglobals
+from GUD2.ORM.copy_number_variant import CNV
+from GUD2.ORM.region import Region
+from GUD2.ORM.source import Source
+
+#-------------#
+# Functions   #
+#-------------#
+
+
+def parse_args():
+    """
+    This function parses arguments provided via the command
+    line using argparse.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="this script inserts short tandem repeat location information")
+
+    parser.add_argument("cvn_file", help="cnv tsv file")
+
+    # Optional args
+    parser.add_argument("--source", default="ISCA-ClinGen", help="Source name")
+
+    # MySQL args
+    mysql_group = parser.add_argument_group("mysql arguments")
+    mysql_group.add_argument("-d", "--db",
+                             help="Database name (default = input genome assembly)")
+    mysql_group.add_argument("-H", "--host", default="localhost",
+                             help="Host name (default = localhost)")
+    mysql_group.add_argument("-P", "--port", default=5506, type=int,
+                             help="Port number (default = 5506)")
+    mysql_group.add_argument("-u", "--user", default=getpass.getuser(),
+                             help="User name (default = current user)")
+
+    args = parser.parse_args()
+
+    # Set default
+    if not args.db:
+        args.db = args.genome
+
+    return args
+
+
+def insert_cnv_to_gud_db(user, host, port, db, tsv_file, source_name):
+
+    # Initialize
+    metadata = {}
+    db_name = "mysql://{}:@{}:{}/{}".format(
+        user, host, port, db)
+    if not database_exists(db_name):
+        raise ValueError("GUD db does not exist: %s" % db_name)
+    session = scoped_session(sessionmaker())
+    engine = create_engine(db_name, echo=False)
+    session.remove()
+    session.configure(bind=engine, autoflush=True,
+                      expire_on_commit=False)
+
+    # Initialize table
+    table = CNV()
+    table.metadata.bind = engine
+    try:
+        table.metadata.create_all(engine)
+    except:
+        raise ValueError("Cannot create \"copy_number_variants\" table!")
+    if not engine.has_table("regions"):
+        raise ValueError("No regions table!")
+    if not engine.has_table("sources"):
+        raise ValueError("No sources table!")
+
+    # get source
+    source = Source()
+    sou = source.select_by_name(session, source_name)
+    if not sou: 
+        source.name = source_name
+        session.merge(source)
+        session.commit()
+        sou = source.select_by_name(session, source_name)
+
+    # parse table
+    with open(tsv_file) as f:
+        if not line.startswith("#"):
+            for line in f:
+                split_line = line.split("\t")
+                split_line[-1] = split_line[-1].rstrip()
+                chrom = str(split_line[0]) 
+                start = int(split_line[1])
+                end = int(split_line[2])
+                clinical_interpretation = str(split_line[4])
+                variant_type = str(split_line[5])
+                copy_number = int(split_line[6])
+                #region entry 
+                region = Region()
+                reg = region.select_by_exact_location(session, chrom, start, end)
+                if not reg: 
+                    region.bin = assign_bin(start, end)
+                    region.chrom = chrom
+                    region.start = start
+                    region.end = end 
+                    session.merge(region)
+                    session.commit()
+                    reg = region.select_by_exact_location(session, chrom, start, end)
+
+                #str entry 
+                cnv = CNV()
+                if cnv.is_unique(session, reg.uid, sou.uid, variant_type, copy_number, clinical_interpretation):
+                    cnv.variant_type = variant_type 
+                    cnv.copy_number = copy_number
+                    cnv.clinical_interpretation = clinical_interpretation
+                    cnv.regionID = reg.uid
+                    cnv.sourceID = sou.uid
+                    session.merge(cnv)
+                    session.commit()
+
+#-------------#
+# Main        #
+#-------------#
+
+
+if __name__ == "__main__":
+
+    # Parse arguments
+    args = parse_args()
+
+    # Insert ENCODE data to GUD database
+    insert_cnv_to_gud_db(args.user, args.host, args.port, args.db,
+                         args.cvn_file, args.source)
