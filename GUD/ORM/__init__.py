3809e5f0393648e732a6b90af538753ac503ac5b
Oriol Fornés <oriol@cmmt.ubc.ca>
2019-02-21 14:27:40 -0800

    updating GUD version 1.0

--- GUD/ORM/__init__.py
+++ GUD/ORM/__init__.py
@@ -8,13 +8,13 @@ __all__ = [
     "tad", "tf_binding", "tss"
 ]
 
-from chrom_size import ChromSize
-from conservation import Conservation
-from dna_accessibility import DnaAccessibility
-from enhancer import Enhancer
-from gene import Gene
-from histone_modification import HistoneModification
-from repeat_mask import RepeatMask
-from tad import Tad
-from tf_binding import TfBinding
+from .chrom_size import ChromSize
+from .conservation import Conservation
+from .dna_accessibility import DnaAccessibility
+from .enhancer import Enhancer
+from .gene import Gene
+from .histone_modification import HistoneModification
+from .repeat_mask import RepeatMask
+from .tad import Tad
+from .tf_binding import TfBinding
 #from .tss import TSS
