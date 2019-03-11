e0aecb1fddf73231b332b738d1d9b6802a99d33b
Oriol Fornés <oriol@cmmt.ubc.ca>
2019-03-11 11:18:40 -0700

    added expression table to ORM

---
+++ GUD2/ORM/expression.py
@@ -0,0 +1,112 @@
+from sqlalchemy import (
+    Column, Index, PrimaryKeyConstraint, String, ForeignKey,
+    UniqueConstraint, Integer, Float, and_, or_
+)
+from sqlalchemy.dialects import mysql
+from GUD2.ORM.region import Region
+from GUD2.ORM.source import Source
+from GUD2.ORM.sample import Sample
+from GUD2.ORM.experiment import Experiment
+from GUD2.ORM.gene import Gene 
+from GUD2.ORM.base import Base
+from binning import containing_bins, contained_bins
+
+class Expression(Base):
+
+    __tablename__ = "expression"
+
+    uid = Column("uid", mysql.INTEGER(unsigned=True))
+    tssID = Column("tssID", Integer, ForeignKey("tss.uid"), nullable=False)
+    sampleID = Column("sampleID", Integer, ForeignKey("samples.uid"), nullable=False)
+    avg_tpm = Column("avg_tpm", Float, nullable=False)
+
+    __table_args__ = (
+        PrimaryKeyConstraint(uid),
+        UniqueConstraint(tssID, sampleID),
+        Index("ix_expression", tssID),
+        Index("ix_expression_sample", sampleID),
+        {
+            "mysql_engine": "MyISAM",
+            "mysql_charset": "utf8"
+        }
+    )
+
+    @classmethod
+    def is_unique(cls, session, tssID, sampleID):
+
+        q = session.query(cls).\
+            filter(
+                cls.tssID == tssID,
+                cls.sampleID == sampleID
+            )
+
+        return len(q.all()) == 0
+
+    @classmethod
+    def select_by_sample(cls, session, sample, min_tpm=10.0):
+
+        q = session.query(cls).\
+            filter(
+                cls.sampleID == sample,
+                cls.avg_tpm >= min_tpm
+            )
+
+        return q.first()
+
+    @classmethod
+    def select_by_samples(cls, session, sample=[], min_tpm=10.0):
+
+        q = session.query(cls).\
+            filter(cls.avg_tpm >= min_tpm).\
+            filter(cls.sampleID.in_(sample))
+
+        return q.all()
+
+    @classmethod
+    def select_by_tss(cls, session, tss, sample=[]):
+        """
+        Query objects by TSS id.
+        """
+
+        q = session.query(cls).filter(cls.tssID = tssID)
+
+        if gene and tss:
+            q = q.filter(cls.gene == gene, cls.tss == tss)
+
+        if sample:
+            q = q.filter(cls.sampleID.in_(sample))
+
+        return q.all()
+
+    @classmethod
+    def select_by_multiple_tss(cls, session, tss=[], sample=[]):
+        """
+        Query objects by multiple TSS ids. If no TSS is
+        provided, return all objects.
+        """ 
+
+        q = session.query(cls)
+
+        if tss:
+            # Initialize
+            ands = []
+            # For each gene, TSS pair...
+            for i, j in tss:
+                ands.append(and_(cls.gene == i,
+                    cls.tss == j))
+            q = q.filter(or_(*ands))
+
+        if sample:
+            q = q.filter(cls.sampleID.in_(sample))
+
+        return q.all()
+
+    @classmethod
+    def get_all_samples(cls, session):
+        """
+        Query all TSS objects in the database and return
+        theirsamples (sampleID field).
+        """
+        samples = session.query(cls.sampleID).distinct().all()
+
+        return [s[0] for s in samples]
