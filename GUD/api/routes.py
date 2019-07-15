e1ffc182cd02581356f2ab12f3069635dcf30b5a
Tamar Av-Shalom <avshalom.tamar0@gmail.com>
2019-07-15 16:16:31 -0700

    setting up home page

--- GUD/api/routes.py
+++ GUD/api/routes.py
@@ -1,8 +1,8 @@
 from GUD.api import app
-from flask import request, jsonify
+from flask import request, jsonify, render_template
 from werkzeug.exceptions import HTTPException, NotFound, BadRequest
 from GUD.api.routes_api import * 
 
 @app.route('/')
 def index():
-    return 'HOME'
+    return render_template('home.html', name='home')
