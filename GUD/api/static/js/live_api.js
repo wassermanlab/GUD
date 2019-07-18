83e378ac636d4824f2ded5e027238873862726f6
Tamar Av-Shalom <avshalom.tamar0@gmail.com>
2019-07-18 11:25:34 -0700

    finishe url building function

--- GUD/api/static/js/live_api.js
+++ GUD/api/static/js/live_api.js
@@ -15,55 +15,87 @@ $(function () {
         var params = resources[resource]['PARAMS']
         for (param in params) {
             var newRow = document.createElement("tr");
-            newRow.id = param + "Row";
+            newRow.id = param;
             $("#paramRows").append(newRow);
-            
+            // create cjeclnpx  box
             var check = document.createElement("td");
             check.id = param + "Check";
-            // <td>
-            //     <div class="form-check">
-            //         <input class="form-check-input"
-            //             type="checkbox" value="" checked="checked" id="rowCheck">
-            //     </div>
-            // </td>
-            if (params[param]['REQUIRED']) {
-                
-            } else {
-
-            }
-            
+            var form_div = document.createElement("div");
+            form_div.id = param + "DivCheck";
+            form_div.classList.add("form-check");
+            var check_input = document.createElement("input");
+            check_input.classList.add("form-check-input");
+            check_input.id = param + "RowCheck";
+            check_input.setAttribute("type", "checkbox");
+            // create key box
             var key = document.createElement("td");
             key.id = param + "Key";
             key.innerHTML = param;
-             // <td>
-            //     <input type="text" class="form-control"
-            //         id="keyInput">
-            // </td>
+            // create value box
             var value = document.createElement("td");
             value.id = param + "Val";
             var input = document.createElement("input");
             input.type = "text";
             input.classList.add("form-control");
             input.id = param + "Input";
-            
+            // create description box
             var desc = document.createElement("td");
             desc.id = param + "Desc";
             desc.innerHTML = params[param]['DESCRIPTION'];
-            
+            // create required  box
             var req = document.createElement("td");
             req.innerHTML = params[param]['REQUIRED'];
             req.id = param + "Req";
-            
-            $("#"+newRow.id).append(check);
-            $("#"+newRow.id).append(key);
-            $("#"+newRow.id).append(value);
-            $("#"+value.id).append(input);
-            $("#"+newRow.id).append(desc);
-            $("#"+newRow.id).append(req);
-
+            // add elements to dom
+            $("#" + newRow.id).append(check);
+            $("#" + check.id).append(form_div);
+            $("#" + form_div.id).append(check_input);
+            if (params[param]['REQUIRED']) {
+                $("#" + check_input.id).prop("checked", true);
+                $("#" + check_input.id).prop("disabled", true);
+            }
+            $("#" + newRow.id).append(key);
+            $("#" + newRow.id).append(value);
+            $("#" + value.id).append(input);
+            $("#" + newRow.id).append(desc);
+            $("#" + newRow.id).append(req);
         }
+        build_url()
     });
 })
+
+// builds url based on selections on page
+function build_url() {
+    url = ""
+    // get selected resource
+    var resource = $("#resourceSelect").val()
+    if (resource == "select") {
+        $("#url").val(url);
+        return;
+    }
+    // for each selected row get key value pair
+    var parameters = {};
+    var params;
+    var check;
+    var key;
+    var val;
+    $("#paramRows").children().each(function (index) {
+        params = this.id;
+        check = $("#" + params + "RowCheck").prop("checked")
+        key = $("#" + params + "Key").text()
+        val = $("#" + params + "Input").val()
+        if (check) {
+            parameters[key] = val;
+        }
+    });
+    // combine and set get request row
+    parameters = Object.entries(parameters);
+    url = resource + "?" + parameters[0][0] + "=" + parameters[0][1];
+    for (var i = 1; i < parameters.length; i++) {
+        url = url + "&" + parameters[i][0] + "=" + parameters[i][1];
+    }
+    $("#url").val(url);
+}
 
 // on selection of parameter 
 // build url
@@ -73,6 +105,7 @@ $(function () {
 
 // on change of parameter value
 // build url
+
 
 // on send 
 
