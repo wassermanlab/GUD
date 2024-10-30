c1780d06a43533a824079d303bbb01353a450902
Brad W <bradw@example.com>
2024-10-29 17:39:10 -0700

    fix utf8 parsing of exons

--- GUD/ORM/gene.py
+++ GUD/ORM/gene.py
@@ -70,14 +70,14 @@ class Gene(GFMixin1, Base):
         exonEnds = []
 
         # For each exon start...
-        for i in str(feat.Gene.exon_starts).split(","):
+        for i in feat.Gene.exon_starts.decode("utf-8").split(","):
             if i.isdigit():
                 exonStarts.append(int(i))
 
         # For each exon end...
-        for i in str(feat.Gene.exon_ends).split(","):
+        for i in feat.Gene.exon_ends.decode("utf-8").split(","):
             if i.isdigit():
-                exonStarts.append(int(i))
+                exonEnds.append(int(i))
 
         # Define qualifiers
         qualifiers = {
@@ -86,8 +86,8 @@ class Gene(GFMixin1, Base):
             "gene_symbol": feat.Gene.gene_symbol,
             "coding_start": int(feat.Gene.coding_start),
             "coding_end": int(feat.Gene.coding_end),
-            "exon_starts": feat.Gene.exon_starts,
-            "exon_ends": feat.Gene.exon_ends,
+            "exon_starts": exonStarts,
+            "exon_ends": exonEnds,
             "source": feat.sourceName,
         }
         genomic_feature = super().as_genomic_feature(feat)
